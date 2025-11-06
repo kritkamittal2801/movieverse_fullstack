51ab11c8020f7dcd1cac66b180723de403f3406b
kritikamittal2801 <kritikamittal2801@gmail.com>
2025-11-06 21:31:15 +0530

    updated app.py

--- backend/app.py
+++ backend/app.py
@@ -459,6 +459,8 @@ def activity():
 def health():
     return "OK", 200
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 10000))  # Render uses dynamic port
+    app.run(host="0.0.0.0", port=port)
 
